1876d92169b048b4b0554c37f08ab52167f73d2c
Kasimashi <Kasimashi@hotmail.com>
2020-10-12 16:48:44 +0200

    Modification Mathieu

--- Java/Javanais/Lettre.java
+++ Java/Javanais/Lettre.java
@@ -1,48 +1,50 @@
 import java.util.*;
 
 public class Lettre {
-	public static void main(String args[]) {}
-	
-	private static Character[] voyelles = {'a', 'e', 'i','o','u','y','A','E','I','O','U','Y'}; 
+
+	private static Character[] voyelles = {'a', 'e', 'i','o','u','y','A','E','I','O','U','Y'};
+	private char lettre;
 
 	/**
 	* Le constructeur de la classe Lettre.
 	*
 	* @param N/A
 	*/
-	 
+
 	 public Lettre() {
+		 lettre=' ';
 	 }
-	
+
+	 public Lettre(char lettre) {a
+		 this.lettre = lettre;
+	 }
+
 	/**
 	* Cette méthode permet de savoir si la lettre est une voyelle.
 	*
 	* @return boolean Vrai si voyelle, faux sinon
 	*/
-	 
-	 public static boolean estVoyelle(char c) {
-	 List<Character> list = Arrays.asList(voyelles);
-	 if (list.contains(c)) {
-		return(true);
-		 }
-		 else {
-			return(false);
-		 }
+
+	 public boolean estVoyelle() {
+	 	List<Character> list = Arrays.asList(this.voyelles);
+		return(list.contains(this.lettre));
 	 }
+
 	/**
 	* Cette méthode permet de savoir si la lettre est un y.
 	*
 	* @return boolean Vrai si y, faux sinon
 	*/
-	 public static boolean estY(char y) {
-		 if (y=='y' || y =='Y') {
-			 return true;
-		 }
-		 return false;
+	 public boolean estY() {
+		 return (this.lettre=='y' || this.lettre=='Y');
 	 }
-	
-	 public void finalize() //Objet récolté par le Garbage Collector --> Le destructeur n'existe pas en Java.
-	 {
-	      System.out.println("Objet nettoyé de la mémoire");  
+
+	 public char getLettre() {
+		 return this.lettre;
 	 }
+	 public void setLettre(char lettre) {
+		 this.lettre=lettre;
+	 }
+
 }
+
--- Java/Javanais/Traducteur.java
+++ Java/Javanais/Traducteur.java
@@ -1,43 +1,43 @@
-import java.awt.*; 
+import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
- 
+
 public class Traducteur extends JFrame implements ActionListener{
-	
+
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 	JButton bouton,effacer,quitter;
 	//Traduction traduction;
 	JTextField text,traduction_text;
-	
+
 	public Traducteur() {
       super();
       this.setSize(800, 500);
       this.setTitle("Javanesse");
-      	
+
       JPanel panel = new JPanel();
       GridLayout gl = new GridLayout(1,3); //1 ligne et n colonnes
       panel.setLayout(gl);
-	  
+
 	  text = new JTextField ("Bonjour !");
 	  traduction_text = new JTextField ("Traduction !");
       bouton = new JButton("Traduire");
-	  effacer = new JButton("Effacer");	  
+	  effacer = new JButton("Effacer");
 	  quitter = new JButton("Quitter");
-	  
-	  
+
+
       panel.add(text);
 	  panel.add(traduction_text);
 	  panel.add(bouton);
 	  panel.add(effacer);
 	  panel.add(quitter);
-	  
+
       bouton.addActionListener(this);
 	  effacer.addActionListener(this);
 	  quitter.addActionListener(this);
-      	         
+
       this.setDefaultCloseOperation(EXIT_ON_CLOSE);
       this.setContentPane(panel);
       this.setVisible(true);
@@ -57,48 +57,54 @@ public class Traducteur extends JFrame implements ActionListener{
          System.out.println("Quitter");
 		 System.exit(0);
       }
-             
+
    }
-   
+
    public void Traduire(String texte){
 	   String texte_traduit;
 	   StringBuilder temp = new StringBuilder();
-	   
+
 	  for (int i=0; i< texte.length(); i++){
-	  
-	      char lettre_buffer = texte.charAt(i);
-	      
-	      if((lettre_buffer >= 'A' && lettre_buffer <= 'Z') || (lettre_buffer >= 'a' && lettre_buffer <= 'z')){
-	    	  System.out.print(lettre_buffer + " est une lettre et c'est une ");
-	    	  
-	    	  if (Lettre.estVoyelle(lettre_buffer)) { 
-					System.out.println("voyelle");  
-					if (Lettre.estY(lettre_buffer) && Lettre.estVoyelle(texte.charAt(i+1)))   {
-						System.out.println("Y : Suivi d'une voyelle ?"); 
-						//Doing Nothing 
-					}
+
+	      Lettre lettre_buffer = new Lettre(texte.charAt(i));
+        Lettre next_letter = new Lettre(' ');
+        try {
+          next_letter.setLettre(texte.charAt(i+1));
+        }
+        catch(Exception e) {
+
+        }
+
+	      if((lettre_buffer.getLettre() >= 'A' && lettre_buffer.getLettre() <= 'Z') || (lettre_buffer.getLettre() >= 'a' && lettre_buffer.getLettre() <= 'z')){
+	    	  System.out.print(lettre_buffer.getLettre() + " est une lettre et c'est une ");
+
+	    	  if (lettre_buffer.estVoyelle()) {
+  					System.out.println("voyelle");
+  					if (!(lettre_buffer.estY() && i!=texte.length() && next_letter.estVoyelle()))   {
+              if((lettre_buffer.getLettre() >= 'A' && lettre_buffer.getLettre() <= 'Z'))
+  						{
+  							temp.append('A');
+  							temp.append('V');
+  						}
+  						else {
+  							temp.append('a');
+  							temp.append('v');
+  						}
+  					}
 					else {
-						if((lettre_buffer >= 'A' && lettre_buffer <= 'Z'))
-						{
-							temp.append('A');
-							temp.append('V');
-						}
-						else {
-							temp.append('a');
-							temp.append('v');
-						}
+						System.out.println("Y : Suivi d'une voyelle ?");
 					}
 			  }else{
-				  System.out.println("consomne");  
+				  System.out.println("consomne");
 			  }
-	    	  
+
 	      }
 	      else{
-	    	  System.out.println(lettre_buffer + " n'est pas une lettre.");
+	    	  System.out.println(lettre_buffer.getLettre() + " n'est pas une lettre.");
 	      }
-	      
-		  temp.append(lettre_buffer);
-		  
+
+		  temp.append(lettre_buffer.getLettre());
+
 	  }
 	  texte_traduit = temp.toString();
 	  System.out.println(texte_traduit);
@@ -107,6 +113,6 @@ public class Traducteur extends JFrame implements ActionListener{
    }
    public static void main(String args[]) {
       @SuppressWarnings("unused")
-	Traducteur fenetre = new Traducteur(); 
+	Traducteur fenetre = new Traducteur();
    }
 }
