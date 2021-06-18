54350ebbee015af8e372171d0ae7f20ff1a1cbc9
Kasimashi <Kasimashi@hotmail.com>
2021-06-18 11:51:14 +0200

    Ajout bytestobytes

--- C/bytes_to_4bytes/bytes_to_4bytes.c
+++ C/bytes_to_4bytes/bytes_to_4bytes.c
@@ -5,6 +5,8 @@ int main(int argc, char **argv)
 {
     int x = 0;
     unsigned char a[]="\xDE\xAD\xBE\xEF\xDE\xAD\xBE\xEF";
+    
+    printf("Addresse de x : %p Valeur de x : %d Addresse de a : %p Valeur de a: %x\n",&x,x,&a,a[0]);
 
     int j;
     for (j=0;j<strlen(a);j+=4){
