3a8d43cc2761d4486249283f01d0d970877e7dc6
Kasimashi <Kasimashi@hotmail.com>
2022-08-12 15:46:28 +0200

    Ajout crc32

--- C/CRC/main.c
+++ C/CRC/main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-#define CRC_SIZE_BLOCK  5
+#define CRC_SIZE_BLOCK  3
 #include "crc16.h"
+#include "crc32.h"
 
 int main(int argc, char **argv)
 {
-    char buff[] = "Helloworlddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd";
-    char * pbuff = buff;
-    unsigned short crc;
+    char buff[] = "Helloworld";
+    char *pbuff = buff;
+    unsigned short u16Crc16 = 0;
+    unsigned long u32Crc32 = 0;
     unsigned int tmp_size, size;
 	
 	size = strlen(buff);
 
 	do{
-	    (size > CRC_SIZE_BLOCK) ? tmp_size = CRC_SIZE_BLOCK 
 	    if (size > CRC_SIZE_BLOCK)
 	    {
 	        tmp_size = CRC_SIZE_BLOCK;
@@ -24,9 +25,12 @@ int main(int argc, char **argv)
 	        tmp_size = size;
 	    }
 	    
-	    printf("Processing '%.5s',len = %d\n",pbuff, tmp_size);
-	    crc = crc16(crc, pbuff, tmp_size);
-	    printf("CRC16 is %X \n",crc);
+	    printf("Processing '%.10s',len = %d\n",pbuff, tmp_size);
+	    u16Crc16 = crc16(u16Crc16, pbuff, tmp_size);
+	    printf("CRC16 is %X \n",u16Crc16);
+	    u32Crc32 = crc32(u32Crc32, pbuff, tmp_size);
+	    printf("CRC32 is %X \n",u32Crc32);
+	    
 	    
 	    size -= tmp_size;
 	    pbuff += tmp_size;
