d27d9bc8447c2462f268867696924f517669d764
Kasimashi <Kasimashi@hotmail.com>
2022-08-27 10:29:11 +0200

    Ajout design pattern singleton C++

---
+++ C++/Design Pattern - Singleton/singleton.cpp
@@ -0,0 +1,57 @@
+/*
+ * Example of a singleton design pattern.
+ * Copyright (C) 2011 Radek Pazdera
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+
+class Singleton
+{
+    private:
+        /* Here will be the instance stored. */
+        static Singleton* instance;
+
+        /* Private constructor to prevent instancing. */
+        Singleton();
+
+    public:
+        /* Static access method. */
+        static Singleton* getInstance();
+};
+
+/* Null, because instance will be initialized on demand. */
+Singleton* Singleton::instance = 0;
+
+Singleton* Singleton::getInstance()
+{
+    if (instance == 0)
+    {
+        instance = new Singleton();
+    }
+
+    return instance;
+}
+
+Singleton::Singleton()
+{}
+
+int main()
+{
+    //new Singleton(); // Won't work
+    Singleton* s = Singleton::getInstance(); // Ok
+    Singleton* r = Singleton::getInstance();
+
+    /* The addresses will be the same. */
+    std::cout << s << std::endl;
+    std::cout << r << std::endl;
+}
